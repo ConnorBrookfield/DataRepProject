71bb8f1ff70f505b3f7a3551cc942a735f53248a
ConnorBrookfield <37177602+G00361129@users.noreply.github.com>
2019-11-20 12:55:40 +0000

    Removed test code
    Removed test server urls

--- src/backend/server.js
+++ src/backend/server.js
@@ -34,22 +34,6 @@ const albumSchema = new Schema({
 
 const AlbumModel = mongoose.model('album', albumSchema);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/whatever', (req, res) => {
-    res.send('whatever')
-})
-
-app.get('/name', (req, res) => {
-    console.log(req.query.lastname)
-    res.send('Welcome ' + req.query.firstname + ' ' + req.query.lastname);
-})
-
-app.post('/name', (req, res) => {
-    console.log(req.body.lastname);
-    res.send('post recieved from ' + req.body.firstname + ' ' + req.body.lastname)
-})
-
 app.get('/albumDB', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
@@ -59,24 +43,6 @@ app.get('/api/albums', (req, res) => {
     AlbumModel.find((error, data) =>{
         res.json({albums:data});
     })
-    // const myMovies = [
-    //     {
-    //         "Title": "Avengers: Infinity War",
-    //         "Year": "2018",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    //     },
-    //     {
-    //         "Title": "Captain America: Civil War",
-    //         "Year": "2016",
-    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    //     }
-    // ];
-
-    // res.status(200).json(
-    //     {
-    //         movies: myMovies,
-    //         message: 'Data Sent'
-    //     });
 })
 
 app.get('/api/albums/:id', (req, res)=>{
